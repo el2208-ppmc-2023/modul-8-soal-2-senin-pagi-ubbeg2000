eb3f062e0ff245c9976730258ee203199deff070
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-04-02 19:46:10 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,54 @@
  *   Deskripsi           :
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define ROW 5
 #define COL 5
+
+//algoritma DFS rekursif
+void DFS(int *M, int row, int col, bool* visited) {
+
+	static int rowNbr[] = { 0, -1, 0, 1 };
+	static int colNbr[] = { -1, 0, 1, 0 };
+
+	*((visited+row*COL) + col) = true; //tandai bahwa index row col matriks sudah dicek
+
+	int jalan = 0; //variabel untuk simpan node ini terhubung oleh berapa jalan (nilai matriks =1) 
+	int baris = 0;
+	int kolom = 0;
+	for (int k = 0; k < 4; ++k){
+		baris = row + rowNbr[k];
+		kolom = col + colNbr[k];
+		if ((baris >= 0) && (baris < ROW) && (kolom >= 0) && (kolom < COL) && (*((M+baris*COL) + kolom))){
+			jalan++;
+			if(!*((visited+baris*COL) + kolom)){
+				DFS(M, baris, kolom, visited);
+				if((*((M+baris*COL) + kolom))==0){
+					jalan--;
+				}
+			}
+		}
+	}
+	if ( jalan<2 && (row!=4 || col!=4) && (row!=0 || col!=0)){
+		*((M+row*COL) + col) = 0;
+	}
+}
+void blok_jalan(int *M){
+	bool visited[ROW][COL];
+	memset(visited, 0, sizeof(visited));
+
+	for (int i = 0; i < ROW; ++i){
+		for (int j = 0; j < COL; ++j){
+			if (*((M+i*COL) + j) && !visited[i][j]){ 
+				DFS(M, i, j, (bool*)visited); 
+			}
+		}
+	}
+}
 
 void file_handling(int* matrix) {
 	char namafile[255];
@@ -43,7 +85,6 @@ void file_handling(int* matrix) {
         i++;
 	}
 }
-
 int main()
 {
 	int temp = 0;
@@ -56,7 +97,8 @@ int main()
 		}
 		printf("\n");
 	}
-    //..............................................
+
+	blok_jalan((int *)M);
 	printf("Matriks hasil blok jalan yang tidak terpakai:\n");
 	//matriks setelah diubah blok jalannya
 	for (int i=0; i<ROW; i++){
